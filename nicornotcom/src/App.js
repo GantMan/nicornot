35c1c0e8f63ec5622a5673378e9d4a7a0f49b4d2
Gant Laborde <GantMan@gmail.com>
2018-09-18 09:27:37 -0500

    almost working

--- nicornotcom/src/App.js
+++ nicornotcom/src/App.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import logo from './NicOrNot.png'
-import './App.css';
+import './App.css'
+import * as tf from '@tensorflow/tfjs'
 
+let nicornot
 class App extends Component {
+  state = {
+    classification: 'Loading'
+  }
+
+  preprocces = async (img) => {
+    nicornot = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json')
+    let tensor = tf.fromPixels(img).toFloat();
+
+    const offset = tf.scalar(127.5);
+    // Normalize the image
+    const normalized = tensor.sub(offset).div(offset);
+
+    // We add a dimension to get a batch shape [1,224,224,3]
+    // batched image
+    return normalized.expandDims(0)
+  }
+
+  predict = (input) => {
+    // get prediction
+    let prediction = nicornot.predict(input)
+    // retrieve the highest probability class label idx
+    console.log(prediction.argMax().buffer().values)
+    return prediction.argMax().buffer().values[0]
+  }
+
+  async componentDidMount () {
+    let img = new Image();
+    img.crossOrigin = '*';
+    img.src = 'https://i.imgur.com/p2mewNT.jpg';
+    img.width = 224;
+    img.height = 224;
+    let classification = await this.preprocces(img)
+    this.setState({classification: this.predict(classification)})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> <em>Coming Soon</em> </h1>
+          <h1 className="App-title"> <em>{this.state.classification}</em> </h1>
         </header>
         <div id="footer">
           <ul>
