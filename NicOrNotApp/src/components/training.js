caff6c8b4352b161455a7e0e32fbfcc4ccd2deb7
Gant Laborde <GantMan@gmail.com>
2018-09-22 19:30:47 -0500

    cleaning and easing

--- NicOrNotApp/src/components/training.js
+++ NicOrNotApp/src/components/training.js
@@ -5,33 +5,15 @@ import {
   View,
   Image,
   Animated,
-  Modal
+  Easing,
+  Modal,
+  TouchableOpacity
 } from 'react-native'
-
-const dark = '#222'
-const accent = '#66b7d8'
-const background = '#4576b9'
-const light = '#fff'
-const scale = 100
-const duration = 1000
-const delayGap = 700
-const notNicImages = [
-  require('../images/headshots/not1.png'),
-  require('../images/headshots/not2.png'),
-  require('../images/headshots/not3.png'),
-  require('../images/headshots/not4.png'),
-  require('../images/headshots/not5.png'),
-  require('../images/headshots/not6.png'),
-  require('../images/headshots/not7.png'),
-  require('../images/headshots/not8.png'),
-  require('../images/headshots/not9.png'),
-  require('../images/headshots/not10.png'),
-  require('../images/headshots/not11.png'),
-  require('../images/headshots/not12.png'),
-  require('../images/headshots/not13.png'),
-  require('../images/headshots/not14.png'),
-  require('../images/headshots/not15.png'),
-]
+import { shuffleArray } from '../lib/shuffle'
+import { metrics, colors, notNicImages } from '../theme'
+const { dark, accent, background, light } = colors
+const { scale, duration, delayGap } = metrics
+const nicImg = require('../images/headshots/nic.png')
 
 export default class App extends Component {
   state = {
@@ -40,88 +22,100 @@ export default class App extends Component {
   }
   // do not use Array(4).fill(new Animated.Value(0)), it shares the instance
   animatedValuesIn = [new Animated.Value(0), new Animated.Value(0), new Animated.Value(0), new Animated.Value(0)]
-  applySettings = val => Animated.timing(
-    val,
-    {
-      toValue: 1,
-      duration,
-      useNativeDriver: true
-    }
-  )
-  animations = this.animatedValuesIn.map(v => this.applySettings(v))
+  animatedValuesOut = [new Animated.Value(1)]
+  spin = this.animatedValuesOut[0].interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '720deg']
+  })
 
   componentDidMount () {
-    Animated.stagger(delayGap, this.animations).start()
+    this.animateIn()
   }
 
-  // Frustrating rewrite of simple array shuffle
-  // oh JavaScript, when will you give us the basics
-  shuffleArray = (a) => {
-    for (let i = a.length - 1; i > 0; i--) {
-      // this semicolon is essential!?!?!?!?!
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]]
-    }
-    return a
+  animateIn = () => {
+    this.animatedValuesOut[0].setValue(1) // reset spin-out
+    const animations = this.animatedValuesIn.map(val => Animated.spring(
+      val,
+      {
+        speed: 2,
+        toValue: 1,
+        bounciness: 12,
+        duration,
+        useNativeDriver: true
+      })
+    )
+    Animated.stagger(delayGap, animations).start()
+  }
+
+  animateOut = (callback) => {
+    this.animatedValuesIn.map(val => Animated.timing(
+      val,
+      {
+        toValue: 0,
+        duration,
+        easing: Easing.cubic,
+        useNativeDriver: true
+      }).start()
+    )
+
+    Animated.timing(
+      this.animatedValuesOut[0],
+      {
+        toValue: 0,
+        duration,
+        easing: Easing.cubic,
+        useNativeDriver: true
+      }).start(callback)
   }
 
   // 3 random pics and 1 nic
   nicPics = () => {
     let pics = []
     while(pics.length < 3){
-        const rando = Math.floor(Math.random()*15)
+        const rando = Math.floor(Math.random()*notNicImages.length)
         if(pics.indexOf(rando) > -1) continue
         pics[pics.length] = rando
     }
 
-    return this.shuffleArray([
+    return shuffleArray([
       notNicImages[pics[0]],
       notNicImages[pics[1]],
       notNicImages[pics[2]],
-      require('../images/headshots/nic.png')
+      nicImg
     ])
   }
+
+  picClick = (idx) => {
+    // window.alert(idx)
+    this.animateOut(this.animateIn)
+  }
+
+  renderSingleOrb = (idx, img) =>
+    <TouchableOpacity key={`orb${idx}`} onPress={() => this.picClick(idx)}>
+      <Animated.View style={[styles.orb, {
+        opacity: this.animatedValuesIn[idx],
+        transform: [
+          {scaleX: this.animatedValuesIn[idx]},
+          {scaleY: this.animatedValuesOut[0]},
+          {rotate: this.spin}
+        ]
+      }]}>
+        <Image style={styles.headshot} source={img} />
+      </Animated.View>
+
+    </TouchableOpacity>
 
   renderOrbs = () => {
     const imageSource = this.nicPics()
     return (
       <View style={styles.midContainer}>
         <View style={styles.imgRow}>
-          <Animated.View style={[styles.orb, {
-            opacity: this.animatedValuesIn[0],
-            transform: [
-              {scaleX: this.animatedValuesIn[0]},
-              // {rotate: spin}
-            ]
-          }]}>
-            <Image style={styles.headshot} source={imageSource[0]} />
-          </Animated.View>
-          <Animated.View style={[styles.orb, {
-            opacity: this.animatedValuesIn[1],
-            transform: [
-              {scaleX: this.animatedValuesIn[1]}
-            ]
-          }]}>
-            <Image style={styles.headshot} source={imageSource[1]} />
-          </Animated.View>
+          {this.renderSingleOrb(0, imageSource[0])}
+          {this.renderSingleOrb(1, imageSource[1])}
         </View>
         <View style={styles.imgRow}>
-          <Animated.View style={[styles.orb, {
-            opacity: this.animatedValuesIn[2],
-            transform: [
-              {scaleX: this.animatedValuesIn[2]}
-            ]
-          }]}>
-            <Image style={styles.headshot} source={imageSource[2]} />
-          </Animated.View>
-          <Animated.View style={[styles.orb, {
-            opacity: this.animatedValuesIn[3],
-            transform: [
-              {scaleX: this.animatedValuesIn[3]}
-            ]
-          }]}>
-            <Image style={styles.headshot} source={imageSource[3]} />
-          </Animated.View>
+          {this.renderSingleOrb(2, imageSource[2])}
+          {this.renderSingleOrb(3, imageSource[3])}
         </View>
       </View>
     )
@@ -132,10 +126,6 @@ export default class App extends Component {
   }
 
   render() {
-    // const spin = this.animatedValuesIn[2].interpolate({
-    //   inputRange: [0, 1],
-    //   outputRange: ['0deg', '360deg']
-    // })
     return (
       <Modal
         animationType="fade"
@@ -150,9 +140,7 @@ export default class App extends Component {
           </Text>
 
           {this.renderTest()}
-          <View style={styles.bar}>
-
-          </View>
+          <View style={styles.bar} />
         </View>
       </Modal>
     )
