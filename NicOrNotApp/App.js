c75e705eee5b193737adc052537f92303bc178ab
Gant <GantMan@gmail.com>
2018-09-24 09:44:36 -0500

    fix yellobox madness

--- NicOrNotApp/App.js
+++ NicOrNotApp/App.js
@@ -1,7 +1,11 @@
-import React from 'react'
 import { createBottomTabNavigator } from 'react-navigation'
 import FinderScreen from './src/components/finderScreen'
 import SettingsScreen from './src/components/settingsScreen'
+import { YellowBox } from 'react-native'
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader'
+])
 
 export default createBottomTabNavigator({
   Finder: FinderScreen,
