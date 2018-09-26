255b2a15a7197055686c846438ff164a91886098
Gant <GantMan@gmail.com>
2018-09-26 09:22:56 -0700

    complete decent settings screen look

--- NicOrNotApp/src/components/settingsScreen.js
+++ NicOrNotApp/src/components/settingsScreen.js
@@ -6,76 +6,119 @@ import {
   View,
   SafeAreaView,
   TouchableOpacity,
-  ScrollView
+  Image
 } from 'react-native'
 import { SocialButton } from '../components/social-button'
 import { colors } from '../theme'
+import Accordion from 'react-native-collapsible/Accordion'
+const logo = require('../images/NicOrNot.png')
 
 export default class App extends Component {
+  state = {
+    statusMessage: '',
+    activeSections: [0]
+  }
+
+  renderHeader = section => {
+    return <Text style={styles.header}>{section}</Text>
+  }
+
+  renderSettings = () => (
+    <View>
+      <Text style={styles.paragraph}>Use your own CoreML model on faces.</Text>
+      <TextInput
+        style={styles.inputModel}
+        placeholder="Full URL to your CoreML model"
+      />
+      <TextInput
+        style={styles.inputModel}
+        placeholder="Class string to match"
+      />
+      <View style={styles.buttonRow}>
+        <TouchableOpacity style={styles.pressy}>
+          <Text style={styles.paragraph}>Fetch</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.reset}>
+          <Text style={styles.paragraph}>Reset to Nic</Text>
+        </TouchableOpacity>
+        <Text style={styles.paragraph}>{this.state.statusMessage}</Text>
+      </View>
+    </View>
+  )
+
+  renderAboutApp = () => (
+    <View>
+      <Text style={styles.paragraph}>
+        Ridiculous? <Text style={styles.link}>Yes!</Text> However, it's a fun
+        adventure into the modern capabilities of Machine Learning. The story of
+        this app, and all its code are available for free.
+      </Text>
+      <Text style={styles.paragraph}>
+        This app coded by Gant Laborde and help from Frank von Hoven and Ray
+        Deck.
+      </Text>
+    </View>
+  )
+
+  renderAboutUs = () => (
+    <View>
+      <Text style={[styles.us, { color: colors.ir }]}>
+        We are Infinite Red.
+      </Text>
+      <Text style={styles.paragraph}>
+        We solve deeply complicated tasks in a way that make problems
+        approachable and fun. If you want to write mobile apps and cool
+        websites, we provide consulting and training.
+      </Text>
+
+      <View style={styles.social}>
+        <SocialButton preset="website" link={'https://infinite.red'} />
+        <SocialButton
+          preset="twitter"
+          link={'https://twitter.com/infinite_red'}
+        />
+        <SocialButton preset="github" link={'https://github.com/infinitered'} />
+        <SocialButton preset="medium" link={'https://shift.infinite.red'} />
+        <SocialButton
+          preset="dribbble"
+          link={'https://dribbble.com/infinitered'}
+        />
+        <SocialButton
+          preset="instagram"
+          link={'https://instagram.com/infinitered_designers'}
+        />
+        <SocialButton
+          preset="facebook"
+          link={'https://facebook.com/infiniteredinc'}
+        />
+      </View>
+    </View>
+  )
+
+  renderContent = section => {
+    if (section === 'Settings') {
+      return this.renderSettings()
+    } else if (section === 'About this App') {
+      return this.renderAboutApp()
+    } else if (section === 'About Us') {
+      return this.renderAboutUs()
+    }
+  }
+
+  updateSections = activeSections => this.setState({ activeSections })
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <Text style={styles.header}>About this App</Text>
-          <Text style={styles.paragraph}>
-            Ridiculous? Yes. However, it's a fun adventure into the modern
-            capabilities of Machine Learning. Why this app exists, and all its
-            code are available for free.
-          </Text>
-          <Text style={[styles.us, { color: colors.ir }]}>
-            We are Infinite Red.
-          </Text>
-          <View style={styles.social}>
-            <SocialButton preset="website" link={'https://infinite.red'} />
-            <SocialButton
-              preset="twitter"
-              link={'https://twitter.com/infinite_red'}
-            />
-            <SocialButton
-              preset="github"
-              link={'https://github.com/infinitered'}
-            />
-            <SocialButton preset="medium" link={'https://shift.infinite.red'} />
-            <SocialButton
-              preset="dribbble"
-              link={'https://dribbble.com/infinitered'}
-            />
-            <SocialButton
-              preset="instagram"
-              link={'https://instagram.com/infinitered_designers'}
-            />
-            <SocialButton
-              preset="facebook"
-              link={'https://facebook.com/infiniteredinc'}
-            />
-          </View>
-          <Text style={styles.paragraph}>
-            We solve deeply complicated tasks in a way that make them
-            approachable and fun. Want to write mobile apps and cool websites?
-            We provide consulting and training.
-          </Text>
-          <Text style={styles.paragraph}>
-            Specifically coded by <Text style={styles.link}>Gant Laborde </Text>
-            and help from
-            <Text style={styles.link}> Frank von Hoven</Text> and
-            <Text style={styles.link}> Ray Deck</Text>.
-          </Text>
-
-          <Text style={styles.header}>Settings</Text>
-          <Text style={styles.paragraph}>
-            Use your own CoreML model on faces.
-          </Text>
-          <TextInput
-            style={styles.inputModel}
-            placeholder="Full URL to your CoreML model"
-          />
-          <View style={styles.social}>
-            <TouchableOpacity style={styles.pressy}>
-              <Text style={styles.paragraph}>Fetch and Compile</Text>
-            </TouchableOpacity>
-            <Text>{}</Text>
-          </View>
-        </ScrollView>
+        <Image source={logo} style={styles.logo} />
+        <Accordion
+          activeSections={this.state.activeSections}
+          sections={['Settings', 'About this App', 'About Us']}
+          touchableComponent={TouchableOpacity}
+          renderHeader={this.renderHeader}
+          renderContent={this.renderContent}
+          onChange={this.updateSections}
+        />
       </SafeAreaView>
     )
   }
@@ -83,12 +126,18 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: colors.dark
   },
+  logo: {
+    height: 150,
+    resizeMode: 'contain'
+  },
   link: {
-    color: colors.accent
+    color: colors.accent,
+    fontWeight: 'bold'
   },
   social: {
     flexDirection: 'row',
@@ -125,6 +174,14 @@ const styles = StyleSheet.create({
     paddingTop: 10
   },
   pressy: {
-    backgroundColor: colors.accent
+    backgroundColor: colors.accent,
+    margin: 5
+  },
+  reset: {
+    backgroundColor: colors.ir,
+    margin: 5
+  },
+  buttonRow: {
+    flexDirection: 'row'
   }
 })
