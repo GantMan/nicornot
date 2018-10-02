4bb555529d8ca18bec7d5b44e1214dfd84b91fef
Gant <GantMan@gmail.com>
2018-10-01 22:48:03 -0500

    clean up semis

--- nicornotcom/src/App.js
+++ nicornotcom/src/App.js
@@ -5,78 +5,77 @@ import * as tf from '@tensorflow/tfjs'
 import { loadFrozenModel } from '@tensorflow/tfjs-converter'
 
 const OPTIONS = ['Nic', 'Not']
-const MODEL_URL = 'tensorflowjs_model.pb';
-const WEIGHTS_URL = 'weights_manifest.json';
+const MODEL_URL = 'tensorflowjs_model.pb'
+const WEIGHTS_URL = 'weights_manifest.json'
 
 class App extends Component {
   state = {
-    classification: "Loading"
-  };
+    classification: 'Loading'
+  }
 
   preprocces = img => {
-    const tensor = tf.fromPixels(img).toFloat();
+    const tensor = tf.fromPixels(img).toFloat()
 
-    const offset = tf.scalar(127.5);
+    const offset = tf.scalar(127.5)
     // Normalize the image
-    const normalized = tensor.sub(offset).div(offset);
+    const normalized = tensor.sub(offset).div(offset)
 
     // We add a dimension to get a batch shape [1,224,224,3]
     // batched image
-    return normalized.expandDims(0);
-  };
+    return normalized.expandDims(0)
+  }
 
   preproccesNic = img => {
-    const tensor = tf.fromPixels(img).toFloat();
+    const tensor = tf.fromPixels(img).toFloat()
 
-    const offset = tf.scalar(127.5);
+    const offset = tf.scalar(127.5)
     // Normalize the image
-    const normalized = tensor.sub(offset).div(offset);
+    const normalized = tensor.sub(offset).div(offset)
 
     // We add a dimension to get a batch shape [1,227,227,3]
     // batched image
-    return normalized.expandDims(0);
-  };
+    return normalized.expandDims(0)
+  }
 
   predict = (input, model) => {
     // get prediction
-    const prediction = model.predict(input);
+    const prediction = model.predict(input)
     // retrieve the highest probability class label idx
-    return prediction.argMax(1).buffer().values[0];
-  };
-
-  loadMobileNet = async (img) => {
-      const mobilenet = await tf.loadModel(
-        "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json"
-      );
-      const cls = this.preprocces(img);
-      return this.predict(cls, mobilenet)
+    return prediction.argMax(1).buffer().values[0]
   }
 
-  loadNicNet = async (img) => {
+  loadMobileNet = async img => {
+    const mobilenet = await tf.loadModel(
+      'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json'
+    )
+    const cls = this.preprocces(img)
+    return this.predict(cls, mobilenet)
+  }
+
+  loadNicNet = async img => {
     let nicmodel
     try {
-      nicmodel = await loadFrozenModel(MODEL_URL, WEIGHTS_URL);
+      nicmodel = await loadFrozenModel(MODEL_URL, WEIGHTS_URL)
       // return nicmodel.execute({input: tf.fromPixels(img)});
-      const cls = this.preproccesNic(img);
+      const cls = this.preproccesNic(img)
       return this.predict(cls, nicmodel)
-    } catch(e) {
+    } catch (e) {
       window.alert('Totes got an error')
       console.log(e)
     }
-
   }
 
   componentDidMount() {
-    let img = new Image();
-    img.crossOrigin = "*";
-    // img.width = 224;
-    // img.height = 224;
-    img.width = 227;
-    img.height = 227;
+    let img = new Image()
+    img.crossOrigin = '*'
+    // img.width = 224
+    // img.height = 224
+    img.width = 227
+    img.height = 227
     // img.src = "https://i.imgur.com/p2mewNT.jpg"; // not
     // img.src = "https://i.imgur.com/BPLtsDR.jpg"; // not
     // img.src = "https://i.imgur.com/IYNZ3UN.jpg" // not
-    img.src = "https://i.imgur.com/fV7Sm6s.jpg" // nic
+    img.src = 'https://i.imgur.com/fV7Sm6s.jpg' // nic
     // img.src = "https://i.imgur.com/FQWxcKg.jpg" // nic
     // img.src = "https://i.imgur.com/5wJryHC.jpg" // nic
     // img.src = "https://i.imgur.com/fUm1zSu.jpg" // not
@@ -84,12 +83,12 @@ class App extends Component {
     // img.src = "https://i.imgur.com/WWXVhFX.jpg" // not
     // img.src = "https://i.imgur.com/qg4a4oU.jpg" // not
 
-
     img.onload = async () => {
       // const result = await this.loadMobileNet(img)
       const result = await this.loadNicNet(img)
-      this.setState({ classification: OPTIONS[result] });
-    };
+      // this.setState({ classification: result })
+      this.setState({ classification: OPTIONS[result] })
+    }
   }
 
   render() {
@@ -97,20 +96,30 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> <em>{this.state.classification}</em> </h1>
+          <h1 className="App-title">
+            {' '}
+            <em>{this.state.classification}</em>{' '}
+          </h1>
         </header>
         <div id="footer">
           <ul>
             <li>Copyright Now(ish)</li>
-            <li><a href="http://declarationofindependencethief.com/">Declaration of Independence Thief Site</a></li>
-            <li><a href="https://github.com/gantman/nicornot">GitHub Repo</a></li>
-            <li><a href="https://slides.com/gantlaborde/cage#/">Slides</a></li>
+            <li>
+              <a href="http://declarationofindependencethief.com/">
+                Declaration of Independence Thief Site
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/gantman/nicornot">GitHub Repo</a>
+            </li>
+            <li>
+              <a href="https://slides.com/gantlaborde/cage#/">Slides</a>
+            </li>
           </ul>
         </div>
-
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
