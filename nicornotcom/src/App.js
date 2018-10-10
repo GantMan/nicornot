fd44feefb683baa5292d2b9e9031a2797e2da970
Gant <GantMan@gmail.com>
2018-10-09 20:52:28 -0500

    update .com to link to app store

--- nicornotcom/src/App.js
+++ nicornotcom/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import logo from './NicOrNot.png'
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
@@ -8,20 +8,63 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> <em>Coming Soon</em> </h1>
         </header>
-        <div id="footer">
+        <div>
+          <p id="opener">
+            Identify if a person <em>is</em> or <em>is NOT</em> Nicolas Cage
+            with ease.
+          </p>
+          <img src="nic_clip.gif" />
+          <p>
+            This app is very useful when{' '}
+            <a
+              href="http://declarationofindependencethief.com/"
+              target="_blank"
+            >
+              someone steals the Declaration of Independence
+            </a>
+            , or if you're just looking to{' '}
+            <a
+              href="https://shift.infinite.red/cage-against-the-machine-a419b6980424"
+              target="_blank"
+            >
+              learn more about facial recognition
+            </a>
+            . If either of those are your need, then this is the app for you!{' '}
+            <a
+              href="https://itunes.apple.com/us/app/nic-or-not/id1437819644?ls=1&mt=8"
+              target="_blank"
+            >
+              This App is <strong>currently available on iOS App Store</strong>
+            </a>
+            .
+          </p>
+          <img src="app.png" className="appStore" alt="app store image" />
+        </div>
+        <footer id="footer">
           <ul>
             <li>Copyright Now(ish)</li>
-            <li><a href="http://declarationofindependencethief.com/">Declaration of Independence Thief Site</a></li>
-            <li><a href="https://github.com/gantman/nicornot">GitHub Repo</a></li>
-            <li><a href="https://slides.com/gantlaborde/cage#/">Slides</a></li>
+            <li>
+              <a href="http://declarationofindependencethief.com/">
+                Declaration of Independence Thief Site
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/gantman/nicornot">GitHub Repo</a>
+            </li>
+            <li>
+              <a href="https://shift.infinite.red/cage-against-the-machine-a419b6980424">
+                Blog Post
+              </a>
+            </li>
+            <li>
+              <a href="https://slides.com/gantlaborde/cage#/">Slides</a>
+            </li>
           </ul>
-        </div>
-
+        </footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
