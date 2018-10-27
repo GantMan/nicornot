9a3982bf86f59a3a3dba9c5434b45cc9488d6ed3
Gant <GantMan@gmail.com>
2018-10-27 13:48:22 -0500

    using new drag/drop

--- nicornotcom/src/App.js
+++ nicornotcom/src/App.js
@@ -1,22 +1,78 @@
 import React, { Component } from 'react'
 import logo from './NicOrNot.png'
 import './App.css'
+import * as faceapi from 'face-api.js'
+import Dropzone from 'react-dropzone'
 
 class App extends Component {
+  state = {
+    classification: 'Loading',
+    currentImage: null,
+    faces: [],
+    files: [],
+    thing: logo
+  }
+
+  async componentDidMount() {
+    await faceapi.loadFaceRecognitionModel('/face_model')
+    this.setState({
+      classification: 'done loading model'
+    })
+    const nic = await faceapi.fetchImage('https://i.imgur.com/DbWl5xs.jpg')
+    const nicDescript = await faceapi.computeFaceDescriptor(nic)
+
+    // const otherURL = 'https://i.imgur.com/fUm1zSu.jpg' // not
+    // const otherURL = 'https://i.imgur.com/fV7Sm6s.jpg' // nic
+    const otherURL = 'https://i.imgur.com/qg4a4oU.jpg' // not
+
+    const other = await faceapi.fetchImage(otherURL)
+    const otherDescript = await faceapi.computeFaceDescriptor(other)
+
+    const distance = faceapi.round(
+      faceapi.euclideanDistance(nicDescript, otherDescript)
+    )
+
+    this.setState({
+      classification: distance
+    })
+  }
+
+  onDrop = (accepted, rejected) => {
+    if (rejected.length > 0) window.alert('JPG or PNG only plz')
+    this.setState({ files: accepted })
+  }
+
   render() {
+    this.state.files.map(f => {
+      const reader = new FileReader()
+      reader.onload = e => {
+        this.setState({ thing: e.target.result })
+      }
+
+      reader.readAsDataURL(f)
+    })
     return (
       <div className="App">
+        <p id="opener">
+          Identify if a person <em>is</em> or <em>is NOT</em> Nicolas Cage with
+          ease.
+        </p>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Dropzone
+            accept="image/jpeg, image/png"
+            className="photo-box"
+            onDrop={this.onDrop.bind(this)}
+          >
+            <img src={this.state.thing} className="dropped-photo" />
+            <p>Drop your file here or click.</p>
+          </Dropzone>
         </header>
         <div>
-          <p id="opener">
-            Identify if a person <em>is</em> or <em>is NOT</em> Nicolas Cage
-            with ease.
-          </p>
+          <p>{this.state.classification}</p>
+          <h2>Also: Available in Mobile App</h2>
           <img src="nic_clip.gif" />
           <p>
-            This app is very useful when{' '}
+            This is useful when{' '}
             <a
               href="http://declarationofindependencethief.com/"
               target="_blank"
